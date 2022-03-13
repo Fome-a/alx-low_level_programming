be7a8e11e5e1f4ed0580ba01d7e0c24f6e979de9
Fome-a <ofomearubs@gmail.com>
2022-03-13 21:10:28 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,28 +1,12 @@
 #!/bin/bash
-/*
- * ******************************************************************************
- * *                                                                            *
- * *                                                 _____  ______    ____  ___ *
- * *    4-puts.c                                    /  _  \ |    |    \   \/  / *
- * *                                               /  /_\  \|    |     \     /  *
- * *    By: Barahmou <hamabarhamou@gmail.com>     /    |    \    |___  /     \  *
- * *                                              \____|__  /_______ \/___/\  \ *
- * *    Created: 2022-03-10 13:29:43 by Barahmou          \/        \/      \_/ *
- * *    Updated: 2022-03-10 13:29:43 by Barahmou                                *
- * *                                                                            *
- * ******************************************************************************
- * */
-
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * * main - Entry point
- * *
- * * Return: Always 0 (Success)
- * */
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *    */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
-
