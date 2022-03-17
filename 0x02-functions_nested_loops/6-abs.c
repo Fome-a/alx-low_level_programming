f7ba532e56f0d9b86947f10460ceb0ab760a71d4
Fome-a <ofomearubs@gmail.com>
2022-03-17 00:16:35 +0000

    task 7

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,7 @@
 #include "main.h"
-
 /**
  *_abs - a function that computes the absolute value of an integer
  *@n: The number
- *
  *Return: On success 1.
  *On error, -1 is returned, and errno is set appropriately.
  *Returns abs of the number
