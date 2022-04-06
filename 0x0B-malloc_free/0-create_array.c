73c7f9d1ba718b83b76e1361a6e2bcf3ae088021
Fome-a <ofomearubs@gmail.com>
2022-04-06 15:53:28 +0000

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - create an array of chars
+ *
+ * Return: Null if size=0 or Null if it fails
+ *
+ */
+
+char *creat_array (unsigned int size, char c)
+{
+	unsigned int i;
+	char c;
+	char *cr;
+
+	i = 0;
+
+	if (size == 0)
+		return (NULL);
+
+	cr = malloc(sizeof(c) * size)
+	
+	if (cr == 0)
+		return (NULL);
+	for(i = 0; i < size; i++)
+		cr[i] = c;
+
+	return (cr);
+}
+
