ee02d5d0a582f1f3d401461d6b47f0e6419b15ff
Fome-a <ofomearubs@gmail.com>
2022-05-06 11:52:46 +0000

    task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	char ch[57] = "and that piece of art is useful\"- Dora Korpar,2015-10-19 ";
+	char ch[57] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19 ";
 
 	for (i = 0; i < 57; i++)
 		putchar(ch[i]);
