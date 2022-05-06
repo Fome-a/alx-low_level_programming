01d9398c741ec4ca728390e594bf52b10396fae3
Fome-a <ofomearubs@gmail.com>
2022-05-06 12:40:22 +0000

    task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - print string without using puts or printf
@@ -8,14 +9,7 @@
 
 int main(void)
 {
-	int i;
-
-	char ch[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	for (i = 0; i < 59; i++)
-	{
-		putchar(ch[i]);
-	}
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
